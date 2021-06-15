d7344bbb7c9f747ec64aaa35877220a437165363
Crowdbotics <team@crowdbotics.com>
2021-06-15 17:11:37 +0000

    Update models

--- backend/manage.py
+++ backend/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'city_personnel_27996.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "city_personnel_27996.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +17,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
